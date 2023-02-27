5f2ade95e47fca10593170656ce7cddcd4027810
Michal Rusek <michal.rusek@dka.io>
2023-02-27 12:13:17 +0100

    refactor quiz question logic

--- lib/question.dart
+++ lib/question.dart
@@ -2,12 +2,5 @@ class Question {
   String questionText;
   bool questionAnswer;
 
-  Question({String q, bool a}) {
-    questionText = q;
-    questionAnswer = a;
-  }
+  Question(this.questionText, this.questionAnswer);
 }
-Question newQuestion = Question(
-  q: 'You can lead a cow down stairs but not up stairs.',
-  a: false,
-);
